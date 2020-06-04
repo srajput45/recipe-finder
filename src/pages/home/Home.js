6f762704a15597ab9e79376369c10157a95e5eb3
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-06-04 22:53:00 +0530

    Addded pages foler which contain the differecnt pages of the app

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from  'react';
+import './home.css';
+import Header from '../../common/header/Header'
+
+class Home extends Component{
+    render(){
+        return(
+            <div >
+                <Header />
+            </div>
+        )
+    }
+}
+export default Home;
